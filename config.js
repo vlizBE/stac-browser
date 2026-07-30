5069060221ebccbca12cb02418aa028c328911ed
Franziska <franziska.regel@vliz.be>
2026-07-30 10:06:24 +0200

    Testing pathPrefix for live website

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ export default {
   displayOverviewsForChildren: false,
   buildTileUrlTemplate: null,
   getMapSourceOptions: null,
-  pathPrefix: "./xyz",
+  pathPrefix: "",
   historyMode: "history",
   cardViewMode: "cards",
   defaultCollectionSort: "title",
