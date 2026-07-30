cac1ae702fb44c66e02cb5642d4ed0a5fc8b0944
Franziska <franziska.regel@vliz.be>
2026-07-30 09:55:59 +0200

    Testing pathPrefix for live website

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ export default {
   displayOverviewsForChildren: false,
   buildTileUrlTemplate: null,
   getMapSourceOptions: null,
-  pathPrefix: "./",
+  pathPrefix: "./stacbrowser/edito",
   historyMode: "history",
   cardViewMode: "cards",
   defaultCollectionSort: "title",
