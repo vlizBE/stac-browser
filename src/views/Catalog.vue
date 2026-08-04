93be8e281784211f40e5551cc00751dafcf10d8c
Matthias Mohr <m.mohr@moregeo.it>
2026-08-04 17:48:08 +0200

    Fix Collection list (filters, odering) in Collection Search #955 (#959)
    * Don't show inline sort and filter options in Collection Search, Preserve sort in Collection Search #955
    * Consistent use of "collection" and "catalog"

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -50,6 +50,7 @@
         <WidgetHook id="view-catalog-catalogs-start" />
         <Catalogs
           :apiSearch="hasApiCollections" :catalogs="catalogs" :hasMore="hasMore"
+          showControls
           @load-more="loadMoreCollections" @search="searchCollections"
           :loading="Boolean(loadingCollections) || loadingNextCollectionsPage" :loadingMore="loadingCollections === 'more' || loadingNextCollectionsPage"
         />
